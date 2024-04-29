836bc51207e3e457d4a4624faee36fdd6274009a
famgz <famgz@proton.me>
2024-04-29 13:22:20 -0300

    feat: home page with instructions

--- main.py
+++ main.py
@@ -10,9 +10,23 @@ tasks = json_(tasks_path)
 app = Flask(__file__)
 
 
+docs_msg = '''
+<div style="max-width: 1024px; margin: 1rem auto">
+    <h1>Tasks api documentation</h1>
+    <p>Usage:</p>
+    <span>GET all taks: /tasks</span>
+</div>
+'''
+
+
+@app.get('/')
+def home():
+    return docs_msg
+
+
 @app.get('/tasks')
 def get_tasks():
-    return 'Tasks api is running'
+    return tasks
 
 
 if __name__ == '__main__':
