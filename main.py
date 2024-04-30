1c3bb6adf7f022b5e7fb2c06796f48888bb69a66
famgz <famgz@proton.me>
2024-04-30 11:41:00 -0300

    feat: PUT and DELETE methods initial impolementation

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask, abort, request, jsonify, render_template
 from config import tasks_path
 from utils import json_
-from tools import validate_task, convert_to_plain_task
+from tools import validate_task, convert_to_plain_task, parse_task
 import uuid
 
 HOST = '0.0.0.0'
-DEBUG = False
+DEBUG = True
 WRITE_TO_JSON = False
 
 
-tasks = json_(tasks_path)
+_tasks = json_(tasks_path)
 
 app = Flask(__file__)
 app.json.sort_keys = False
@@ -18,7 +18,7 @@ app.json.sort_keys = False
 def save_tasks():
     if not WRITE_TO_JSON:
         return
-    json_(tasks_path, tasks,
+    json_(tasks_path, _tasks,
           ensure_ascii=False, sort_keys=False, indent=2)
 
 
@@ -29,15 +29,15 @@ def home():
 
 @app.get('/tasks')
 def get_tasks():
-    return [convert_to_plain_task(task_id, task_info) for task_id, task_info in tasks.items()]
+    return [convert_to_plain_task(task_id, task_info) for task_id, task_info in _tasks.items()]
 
 
 @app.get('/tasks/<string:task_id>')
 def get_task(task_id):
-    task = tasks.get(task_id)
+    task = _tasks.get(task_id)
     if not task:
-        return jsonify({'message': f'Task not found: {task_id}'}), 422
-    return convert_to_plain_task(task_id, task[task_id])
+        return jsonify({'message': f'Task not found with _id: {task_id}'}), 404
+    return convert_to_plain_task(task_id, _tasks[task_id])
 
 
 @app.post('/tasks')
@@ -48,22 +48,45 @@ def add_task():
         abort(400)
 
     try:
-        validate_task(data)
+        validate_task(data, method=request.method)
+        task_info = parse_task(data)
     except Exception as e:
         return jsonify({'error': str(e)}), 422
 
     task_id = str(uuid.uuid4())
-
-    task = {
-        task_id: {
-            'title':  data.get('title'),
-            'category': data.get('category'),
-            'completed': data.get('completed', False),
-            'dueHour':  data.get('dueHour'), }
-    }
-    tasks.update(task)
+    task = {task_id: task_info}
+    _tasks.update(task)
     save_tasks()
-    return convert_to_plain_task(task), 200
+    return convert_to_plain_task(task_id, task_info), 200
+
+
+@app.put('/tasks/<string:task_id>')
+def update_task(task_id):
+    if task_id not in _tasks:
+        return jsonify({'message': f'Task not found with _id: {task_id}'}), 404
+
+    data = request.json
+
+    if not data:
+        abort(400)
+
+    try:
+        validate_task(data, method=request.method)
+        task_info = parse_task(data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 422
+
+    _tasks[task_id].update(task_info)
+    save_tasks()
+    return convert_to_plain_task(task_id, _tasks[task_id]), 200
+
+
+@app.delete('/tasks/<string:task_id>')
+def delete_task(task_id):
+    if task_id not in _tasks:
+        return jsonify({'message': f'Task not found with _id: {task_id}'}), 404
+    del _tasks[task_id]
+    return jsonify({'message': 'Item deleted successfully'}), 200
 
 
 if __name__ == '__main__':
